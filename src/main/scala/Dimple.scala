7f90e59f4057a28a8be0d6a76d86096609becadd
Babatunde Fashola <bfashola@Babatundes-iMac.local>
2016-06-19 03:29:35 -0700

    Refactoring

--- src/main/scala/Dimple.scala
+++ src/main/scala/Dimple.scala
@@ -10,8 +10,6 @@ import scala.scalajs.js.|
 @js.native
 object Dimple extends js.Object {
 
-  js.Dynamic
-
   def filterData(data: js.Array, field: String, filterValues: String | js.Array[String]): js.Array = js.native
 
   def getUniqueValues(data: js.Array, fields: String | js.Array[String]): js.Array[String] = js.native
