cf6e69e009c2e5be0b911c2bbaab456d5cf91de4
Babatunde Fashola <bfashola@twitter.com>
2016-06-26 09:21:21 -0700

    Changes to the Chart and Dimple objects

--- src/main/scala/Axis.scala
+++ src/main/scala/Axis.scala
@@ -1,42 +1,48 @@
 package org.scala.dimplejs
 
+import org.singlespaced.d3js.timeModule.Interval
 import scala.scalajs.js
 import scala.scalajs.js.`|`
 import scala.scalajs.js.annotation.JSName
+import org.singlespaced.d3js.TimeObject
 
 @JSName("dimple.axis")
 @js.native
 class Axis(
-            chart: Chart,
-            position: String,
-            categoryFields: String | js.Array[String] = null,
-            measure: String = null,
-            timeField: String = null
-          ) extends js.Object {
-  val colors = js.native
-  val clamp = js.native
-  val fontSize = js.native
-  val fontFamily = js.native
-  val gridlineShapes = js.native
-  val hidden = js.native
-  val logBase = js.native
-  val overrideMin = js.native
-  val overrideMax = js.native
-  val shapes = js.native
-  val showGridlines = js.native
-  val showPercent = js.native
-  val titleShape = js.native
-  val tickFormat = js.native
-  val title = js.native
-  val floatingBarWidth = js.native
-  val dateParseFormat = js.native
-  val ticks = js.native
-  val timePeriod = js.native
-  val timeInterval = js.native
-  val useLog = js.native
+  chart: Chart,
+  position: String,
+  categoryFields: String | js.Array[String] = null,
+  measure: String = null,
+  timeField: String = null
+) extends js.Object {
+  val colors: String | js.Array[String] = js.native
+  val clamp: Boolean = js.native
+  val fontSize: String = js.native
+  val fontFamily: String = js.native
+  val gridlineShapes: js.Any = js.native
+  val hidden: Boolean = js.native
+  val logBase: Int = js.native
+  val overrideMin: Float = js.native
+  val overrideMax: Float = js.native
+  val shapes: js.Any = js.native
+  val showGridlines: Boolean = js.native
+  val showPercent: Boolean = js.native
+  val titleShape: js.Any = js.native
+  val tickFormat: String = js.native
+  val title: String= js.native
+  val floatingBarWidth: Int = js.native
+  val dateParseFormat: String = js.native
+  val ticks: Int = js.native
+  val timePeriod: Interval= js.native
+  val timeInterval: Int = js.native
+  val useLog: Boolean = js.native
 
-  def addGroupOrderRule(ordering: String | js.Array[String] | js.Function2[Any, Any, Int], desc: Boolean = false): Unit = js.native
+  def addGroupOrderRule(
+    ordering: String | js.Array[String] | js.Function2[Any, Any, Int],
+    desc: Boolean = false): Unit = js.native
 
-  def addOrderRule(ordering: String | js.Array[String] | js.Function2[Any, Any, Int], desc: Boolean = false): Unit = js.native
+  def addOrderRule(
+    ordering: String | js.Array[String] | js.Function2[Any, Any, Int],
+    desc: Boolean = false): Unit = js.native
 
 }
--- src/main/scala/Chart.scala
+++ src/main/scala/Chart.scala
@@ -1,8 +1,7 @@
 package org.scala.dimplejs
 
-import org.singlespaced.d3js.{SvgObject, Xhr}
+import org.singlespaced.d3js.SvgObject
 import org.singlespaced.d3js.timeModule.FormatObject
-
 import scala.scalajs.js
 import scala.scalajs.js.`|`
 import scala.scalajs.js.annotation.JSName
@@ -10,56 +9,118 @@ import scala.scalajs.js.annotation.JSName
 
 @JSName("dimple.chart")
 @js.native
-class Chart(val svg: SvgObject, val data: Xhr) extends js.Object {
+class Chart(val svg: SvgObject, val data: js.Array[Any]) extends js.Object {
 
-  type Options = String | js.Array[String]
-  type Primitives = String | Int | Double
-  type Series = Series | js.Array[Series]
+  type Stringy = String | js.Array[String]
+  type Primitives = String | Int
+  type Seriez = Series | js.Array[Series]
 
-  val axes = js.native
-  val customeClassList = js.native
-  val defaultColors = js.native
-  val ease = js.native
-  val height = js.native
-  val legends = js.native
-  val noFormats = js.native
-  val series = js.native
-  val staggerDraw = js.native
-  val storyboard = js.native
-  val width = js.native
-  val x = js.native
-  val y = js.native
+  val axes: js.Array[Axis] = js.native
+  val customClassList: js.Any = js.native
+  val defaultColors: js.Array[Color] = js.native
+  val ease: String = js.native
+  val height: Int = js.native
+  val legends: js.Array[Legend] = js.native
+  val noFormats: Boolean = js.native
+  val series: js.Array[Series] = js.native
+  val staggerDraw: Boolean = js.native
+  val storyboard: StoryBoard = js.native
+  val width: Int = js.native
+  val x: Int = js.native
+  val y: Int = js.native
 
-  def addAxis(postion: String, categoryFields: Options = null, measure: String = "", timeFiled: String = ""): Axis = js.native
+  def addAxis(
+    postion: String,
+    categoryFields: Stringy = null,
+    measure: String = "",
+    timeField: String = ""): Axis = js.native
 
-  def addColorAxis(measure: String, colors: Options = null): Axis = js.native
+  def addCategoryAxis(
+    position: String,
+    measure: Stringy
+  ): Axis = js.native
 
-  def addLegend(x: Primitives, y: Primitives, width: Primitives, height: Primitives, horizontalAlign: String = "left", series: Series): Unit = js.native
+  def addColorAxis(
+    measure: String,
+    colors: Stringy = null
+  ): Axis = js.native
 
-  def addLogAxis(position: String, measure: String, logBase: Int = 10): Axis = js.native
+  def addLegend(
+    x: Primitives,
+    y: Primitives,
+    width: Primitives,
+    height: Primitives,
+    horizontalAlign: String = "left",
+    series: Seriez): Unit = js.native
 
-  def addMeasureAxis(position: String, measure: String): Axis = js.native
+  def addLogAxis(
+    position: String,
+    measure: String,
+    logBase: Int = 10
+  ): Axis = js.native
 
-  def addPctAxis(position: String, measure: String, categories: AnyRef): Axis = js.native
+  def addMeasureAxis(
+    position: String,
+    measure: String
+  ): Axis = js.native
 
-  def addTimeAxis(position: String, timeField: String, inputFormat: FormatObject | String = "", outputFormat: FormatObject | String = ""): Axis = js.native
+  def addPctAxis(
+    position: String,
+    measure: String,
+    categories: Stringy = null
+  ): Axis = js.native
 
-  def addSeries(categoryFields: Options = null, plotFunction: js.Function0[Unit], axes: Axis | js.Array[Axis] = null): Series = js.native
+  def addTimeAxis(
+    position: String,
+    timeField: String,
+    inputFormat: FormatObject | String = null,
+    outputFormat: FormatObject | String = null
+  ): Axis = js.native
 
-  def assignClass(tag: String, css: String): String = js.native
+  def addSeries(
+    categoryFields: Stringy = null,
+    plotFunction: Plot,
+    axes: Axis | js.Array[Axis] = null
+  ): Series = js.native
 
-  def assignColor(tag: String, fill: String, stroke: String = "", opacity: Double): Color = js.native
+  def assignClass(
+    tag: String,
+    css: String
+  ): String = js.native
 
-  def draw(duration: Double = 0, noDataChange: Boolean = false): Unit = js.native
+  def assignColor(
+    tag: String,
+    fill: String,
+    stroke: String = "",
+    opacity: Float
+  ): Color = js.native
+
+  def draw(
+    duration: Float = 0,
+    noDataChange: Boolean = false
+  ): Chart = js.native
 
   def getClass(tag: String): String = js.native
 
   def getColor(tag: String): Color = js.native
 
-  def setBounds(x: String | Int, y: String | Int, width: Int | String, height: String | Int): Chart = js.native
+  def setBounds(
+    x: String | Int,
+    y: String | Int,
+    width: Int | String,
+    height: String | Int
+  ): Chart = js.native
 
-  def setMargins(left: String | Int, top: String | Int, right: String | Int, bottom: String | Int): Chart = js.native
+  def setMargins(
+    left: String | Int,
+    top: String | Int,
+    right: String | Int,
+    bottom: String | Int
+  ): Chart = js.native
 
-  def setStoryboard(categoryFields: String, tickHandler: js.Function1[EventArgs, Unit]): StoryBoard = js.native
+  def setStoryboard(
+    categoryFields: String,
+    tickHandler: js.Function1[EventArgs, Unit]
+  ): StoryBoard = js.native
 
 }
