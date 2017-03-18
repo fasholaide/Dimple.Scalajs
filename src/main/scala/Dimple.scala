5cc835289e92ff74c06db13ea1c7020eb343b92c
Babatunde Fashola <bfashola@twitter.com>
2017-03-18 13:46:18 -0700

    Some Formatting Chanages

--- src/main/scala/Dimple.scala
+++ src/main/scala/Dimple.scala
@@ -1,7 +1,6 @@
 package org.scala.dimplejs
 
 import org.singlespaced.d3js.Selection
-
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSName
 import scala.scalajs.js.|
@@ -10,9 +9,19 @@ import scala.scalajs.js.|
 @js.native
 object Dimple extends js.Object {
 
-  def filterData(data: js.Array[js.Dictionary[String]] | js.Array[js.Object with js.Dynamic] , field: String, filterValues: String | js.Array[String]): js.Array[js.Object with js.Dynamic] | js.Array[js.Dictionary[String]] = js.native
+  def filterData(
+    data: js.Array[js.Dictionary[String]] | js.Array[js.Object with js.Dynamic],
+    field: String, filterValues: String | js.Array[String]
+  ): js.Array[js.Object with js.Dynamic] | js.Array[js.Dictionary[String]] = js.native
 
-  def getUniqueValues(data: js.Array[js.Dictionary[String]] | js.Array[js.Object with js.Dynamic] , fields: String | js.Array[String]): js.Array[js.Object with js.Dynamic] | js.Array[js.Dictionary[String]]  = js.native
+  def getUniqueValues(
+    data: js.Array[js.Dictionary[String]] | js.Array[js.Object with js.Dynamic],
+    fields: String | js.Array[String]
+  ): js.Array[js.Object with js.Dynamic] | js.Array[js.Dictionary[String]] = js.native
 
-  def newSvg(parentSelector: String, width: String | Int, height: String | Int): Selection[org.scalajs.dom.EventTarget] = js.native
+  def newSvg(
+    parentSelector: String,
+    width: String | Int,
+    height: String | Int
+  ): Selection[org.scalajs.dom.EventTarget] = js.native
 }
